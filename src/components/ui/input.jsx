088cf18bcd5dd90fcc436af53d7bd4269e1b7f74
ayushsaha1018 <ayushsaha111@gmail.com>
2025-01-12 16:43:30 +0530

    feat: commit improvements

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,19 +1,37 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { Eye, EyeOff } from "lucide-react";
 
-const Input = React.forwardRef(({ className, type, ...props }, ref) => {
+const Input = React.forwardRef(({ className, type, togglePassword = true, ...props }, ref) => {
+  const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
+
+  const toggleVisibility = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
   return (
-    (<input
-      type={type}
-      className={cn(
-        "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        className
+    <div className="relative flex items-center">
+      <input
+        type={togglePassword && isPasswordVisible ? "text" : type}
+        className={cn(
+          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+      {type === "password" && togglePassword && (
+        <button
+          type="button"
+          onClick={toggleVisibility}
+          className="absolute right-3 text-muted-foreground hover:text-foreground focus:outline-none"
+        >
+          {isPasswordVisible ? <EyeOff className="size-4" /> : <Eye className="size-4" />}
+        </button>
       )}
-      ref={ref}
-      {...props} />)
+    </div>
   );
-})
-Input.displayName = "Input"
+});
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
