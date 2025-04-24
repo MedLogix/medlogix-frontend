9ca1ef570e09cd6556f8d400d9f376728cf906c9
ayushsaha1018 <ayushsaha111@gmail.com>
2025-04-24 10:24:30 +0530

    feat: chat

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -3,7 +3,7 @@ import { Bot } from "lucide-react";
 const Chatbot = () => {
   return (
     <a
-      href="https://medicalchatbot-sjui4gwbchhtkiemjzkxsw.streamlit.app/"
+      href="https://medbotinventory.streamlit.app/"
       target="_blank"
       className="fixed bottom-4 right-4 z-50 cursor-pointer rounded-full bg-primary p-4 text-primary-foreground hover:bg-primary/80"
     >
